940d04ebd1d6a31980c03202aeb1d320995b2a2b
Krista Chung <kchung187@gmail.com>
2024-04-30 01:31:55 -0600

    Create functonality for logging in on client side

--- client/src/Pages/SignInPage/SigninPage.js
+++ client/src/Pages/SignInPage/SigninPage.js
@@ -1,13 +1,55 @@
 import axios from "axios";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 const SigninPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8080/api/signin", {
+        email,
+        password,
+      });
+      const { userID } = response.data;
+
+      //Store user id in storage for the session
+      sessionStorage.setItem("userID", userID);
+
+      //Upon successful login, redirect to the beanmap
+      alert("Successful log-in, redirecting to your bean map");
+      setLoggedIn(true);
+    } catch (error) {
+      console.error("Error logging in:", error);
+      setError("Invalid email/password, please try again!");
+    }
+  };
+
+  if (loggedIn) {
+    return <Navigate to="/" />;
+  }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1> Log in to add some beans! </h1>
-        <input name="email" type="text" placeholder="Email" />
-        <input name="password" type="password" placeholder="Password" />
-        <button>Log In</button>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Log In</button>
+        {error && <p>{error}</p>}
       </form>
     </>
   );
